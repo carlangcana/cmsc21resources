40ebad5acf19a0653e3a22eb8bffc9543326db94
carlangcana <cgangcana@up.edu.ph>
2023-07-09 18:59:14 +0800

    chore: initial commit

---
+++ intro/intro.c
@@ -0,0 +1,30 @@
+/*
+* Author: Carl Angelo G. Angcana
+*
+* [Topic for today]
+* 
+* July 7, 2023
+*/
+
+#include<stdio.h> // Preprocessor directive
+
+int main(){
+
+    // int a, b, sum;
+    // printf("Input 2 numbers please!\n");
+    // scanf("%i", &a);
+    // scanf("%i", &b);
+
+    // sum = a + b;
+    // printf("The sum of two numbers: %i\n", sum);
+
+    char name[10];
+    char computer[10] = "computer";
+    printf("What is your name?:");
+    scanf("%s", computer);
+
+    printf("Hi %s!, I am the %s!\n", name, computer);
+
+    // 
+
+}
