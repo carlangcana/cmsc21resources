02444e985586533575e1bbf37c4152a2b6e71232
carlangcana <cgangcana@up.edu.ph>
2023-07-13 23:29:11 +0800

    chore: added arithmetic sequence example in the lesson

---
+++ recursion/arithmetic_sequence.c
@@ -0,0 +1,35 @@
+/****
+ * Author: Carl Angelo G. Angcana
+ * 
+ * Topic: Recursion in C
+ * 
+ * July 13, 2023
+*/
+
+#include<stdio.h>
+
+void arithmetic_sequence(int a, int b, int n);
+
+int main(){
+    int a, b, n;
+
+    printf("Enter the first term: ");
+    scanf("%d", &a);
+    printf("Enter the common difference: ");
+    scanf("%d", &b);
+    printf("Enter the n term: ");
+    scanf("%d", &n);
+
+    arithmetic_sequence(a, b, n);
+    return 0;
+}
+
+void arithmetic_sequence(int a, int b, int n) {
+    if(n == 0){
+        return;
+    }else{
+        a = a + b;
+        printf("%d \n", a);
+        arithmetic_sequence(a, b, (n - 1));
+    }
+}
